a6156a6df9476e66506656e5d2cc4f54ae960505
JesseKnab <jessehuisman80@gmail.com>
2022-11-25 14:28:49 +0100

    improve structure

--- KNAB-Assessment/Tests/APITests.cs
+++ KNAB-Assessment/Tests/APITests.cs
@@ -8,24 +8,33 @@ namespace KNAB_Assessment.Tests
     [TestFixture]
     public class APITests
 	{
-        private string token = "1e3fa39780c1b75c5acb8c3ba9390883";
-        private string appKey = "5b90beb8391be3a47acb3cd6a80aa72564ce397577f1b37d80f3fe4e167d0252";
+        private static string url = "https://api.trello.com";
+        private static string appKey = "1e3fa39780c1b75c5acb8c3ba9390883";
+        private static string token = "5b90beb8391be3a47acb3cd6a80aa72564ce397577f1b37d80f3fe4e167d0252";
+        private static string sep = "&";
+        private static string credentials = "?key=" + appKey + sep + "token=" + token;
+        
+        //operations
+        private static string createCard = url + "/1/cards" + credentials;
 
-        //[SetUp]
-        //public void Setup()
-        //{
-        //}
+        //attributes
+        private static string assignName = sep + "name=";
+
+        //targets
+        private static string onIdList = sep + "idList=";
+
 
         [Test]
         public static void apiTest()
         {
-            //string url = "https://api.trello.com";
-            string url = "https://api.trello.com/1/cards?key=1e3fa39780c1b75c5acb8c3ba9390883&token=5b90beb8391be3a47acb3cd6a80aa72564ce397577f1b37d80f3fe4e167d0252&idList=6380ad590ff28d0c9a276f1d&name=test card visual studio";
-            RestClient client = new RestClient(url);
+            string idList = "6380ad590ff28d0c9a276f1d";
+            string stringToPost = createCard + onIdList + idList + assignName + "test card visual studio 2";
+            Console.WriteLine("posting:" + stringToPost);
+
+            RestClient client = new RestClient(stringToPost);
             RestRequest request = new RestRequest();
             var response = client.Post(request);
-
-            Console.WriteLine(response.Content.ToString());
+            Console.WriteLine("response: " + response.Content.ToString());
             Console.Read();
 
         }
@@ -33,7 +42,7 @@ namespace KNAB_Assessment.Tests
         [TearDown]
         public void TearDown()
         {
-            //remove board, list, card
+            //todo: remove board, list, card
         }
     }
 }
